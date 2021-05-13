02c6cf27750a71e1a7e17c97dc3f92e920333b68
Eray Aydin <erraydin@gmail.com>
2021-05-13 04:36:40 -0500

    stack of strings

---
+++ src/Stack/StackOfStrings.java
@@ -0,0 +1,37 @@
+package Stack;
+import java.util.EmptyStackException;
+
+public class StackOfStrings {
+
+    private Node first = null;
+
+    private static class Node {
+        String item;
+        Node next;
+        public Node(String item) {
+            this.item = item;
+        }
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void push(String item) {
+        Node head = new Node(item);
+        head.next = first;
+        first = head;
+    }
+
+    public String pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        String item = first.item;
+        first = first.next;
+        return item;
+
+    }
+
+}
---
+++ src/Stack/StackTest.java
@@ -0,0 +1,13 @@
+package Stack;
+
+public class StackTest {
+    public static void main(String[] args) {
+        StackOfStrings stack = new StackOfStrings();
+        stack.push("item1");
+        stack.push("item2");
+        System.out.println(stack.pop());
+        stack.push("item3");
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
