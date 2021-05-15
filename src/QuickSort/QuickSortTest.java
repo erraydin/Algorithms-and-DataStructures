97088ec021c2172a9e70daade13b66eefb1803fe
Eray Aydin <erraydin@gmail.com>
2021-05-15 17:24:25 -0500

    quicksort test

--- src/QuickSort/QuickSortTest.java
+++ src/QuickSort/QuickSortTest.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class QuickSortTest {
     public static void main(String[] args) {
-        Integer[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17};
+        Integer[] a = {1, 2, 2, 2, 3, 3, 3, 8, 9, 10, 11, 11, 11, 14, 14, 16,17};
         Shuffle.shuffle(a);
         System.out.println(Arrays.toString(a));
 
