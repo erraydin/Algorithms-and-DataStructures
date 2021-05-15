2134ba9f06a7b3710a182e9adafeb1e6f4d34b35
Eray Aydin <erraydin@gmail.com>
2021-05-15 15:04:13 -0500

    add comparator support to MergeSort

--- src/MergeSort/MergeSort.java
+++ src/MergeSort/MergeSort.java
@@ -1,22 +1,24 @@
 package MergeSort;
 
+import java.util.Comparator;
+
 public class MergeSort {
-    public static void sort(Comparable[] a) {
-        Comparable[] aux = new Comparable[a.length];
-        sortHelper(a, aux, 0, a.length - 1);
+    public static void sort(Object[] a, Comparator comparator) {
+        Object[] aux = new Object[a.length];
+        sortHelper(a, aux, 0, a.length - 1, comparator);
 
     }
 
-    private static void sortHelper(Comparable[] a, Comparable[] aux, int low, int high) {
+    private static void sortHelper(Object[] a, Object[] aux, int low, int high, Comparator comparator) {
         if (high <= low) return;
 
         int mid = low + (high - low) / 2;
-        sortHelper(a, aux, low, mid);
-        sortHelper(a, aux, mid + 1, high);
-        merge(a, aux, low, mid, high);
+        sortHelper(a, aux, low, mid, comparator);
+        sortHelper(a, aux, mid + 1, high, comparator);
+        merge(a, aux, low, mid, high, comparator);
     }
 
-    private static void merge(Comparable[] a, Comparable[] aux, int low, int mid, int high) {
+    private static void merge(Object[] a, Object[] aux, int low, int mid, int high, Comparator comparator) {
 
         for (int i = low; i <= high; i++) {
             aux[i] = a[i];
@@ -28,7 +30,7 @@ public class MergeSort {
         for (int k = low; k<= high; k++) {
             if ( i > mid )   a[k] = aux[j++];
             else if ( j > high )  a[k] = aux[i++];
-            else if (aux[i].compareTo(aux[j]) <= 0) a[k] = aux[i++];
+            else if (comparator.compare(aux[i], aux[j]) <= 0) a[k] = aux[i++];
             else a[k] = aux[j++];
 
         }
--- src/MergeSort/MergeSortTest.java
+++ src/MergeSort/MergeSortTest.java
@@ -1,13 +1,21 @@
 package MergeSort;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class MergeSortTest {
     public static void main(String[] args) {
         Integer[] array  = {5, 11, 0, 3, 4, 8, 1, 9, 2, 7, 6, 10};
 
         System.out.println(Arrays.toString(array));
-        MergeSort.sort(array);
+        MergeSort.sort(array, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 > o2) return 1;
+                else if (o1 < o2) return -1;
+                else return 0;
+            }
+        });
         System.out.println(Arrays.toString(array));
     }
 }
