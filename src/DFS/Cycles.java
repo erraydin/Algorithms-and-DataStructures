201f9359ba432d183b76afdecd83442957b8c68f
Eray Aydin <erraydin@gmail.com>
2021-05-30 01:56:51 -0500

    bipartite

--- src/DFS/Cycles.java
+++ src/DFS/Cycles.java
@@ -4,12 +4,19 @@ import Graph.Graph;
 
 public class Cycles {
 
-    public static boolean hasCycles(Graph graph) {
 
-        boolean[] visited = new boolean[graph.V()];
+    /*#################################################################################################
+    * ##########################  CHECK IF CYCLIC   ###################################################
+    * ################################################################################################*/
+
+    public static boolean hasCycles(Graph graph) {
+        //visited[vertex] == 0 means it's not visited
+        //                == 1 means is being visited
+        //                == 2 means we finished visiting vertex and all the dfs calls from its children
+        int[] visited = new int[graph.V()];
         boolean res = false;
         for (int v = 0; v < graph.V(); v++) {
-            if (!visited[v]) {
+            if (visited[v] == 0) {
                 if(hasCyclesDfs(graph, -1, v, visited)) {
                     res = true;
                     break;
@@ -19,18 +26,63 @@ public class Cycles {
         return res;
     }
 
-    private static boolean hasCyclesDfs(Graph graph, int parent, int cur, boolean[] visited) {
-        visited[cur] = true;
+    private static boolean hasCyclesDfs(Graph graph, int parent, int cur, int[] visited) {
+        visited[cur] = 1;
+        boolean res = false;
         for (int neighbor: graph.adj(cur)) {
-            if (!visited[neighbor]) {
+            if (visited[neighbor] == 0) {
                 if (hasCyclesDfs(graph, cur, neighbor, visited)) {
-                    return true;
+                    res = true;
+                    break;
                 }
-            } else if(neighbor != parent){
-                    return true;
+            } else if(visited[neighbor] == 1 && neighbor != parent){
+                    res = true;
+                    break;
             }
-
         }
-        return false;
+        visited[cur] = 2;
+        return res;
+    }
+    /*---------------------------------------><---------------------------------------------------*/
+
+
+
+    /*#################################################################################################
+     * ##################################  CHECK IF BIPARTITE   #########################################
+     * ################################################################################################*/
+    public static boolean isBipartite(Graph graph) {
+        boolean[] visited = new boolean[graph.V()];
+
+        // colors are 0 and 1
+        int[] color = new int[graph.V()];
+        boolean res = true;
+        for (int v = 0; v < graph.V(); v++){
+            if (!visited[v]) {
+                if(!dfsIsBipartite(graph, -1, v, visited, color)) {
+                    res = false;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    private static boolean dfsIsBipartite(Graph graph, int parent, int cur, boolean[] visited, int[] color) {
+        visited[cur] = true;
+        if (parent == -1) color[cur] = 0;
+        else color[cur] = 1 - color[parent];
+        boolean res = true;
+        for (int neighbor : graph.adj(cur)) {
+            if (!visited[neighbor]) {
+                if (!dfsIsBipartite(graph, cur, neighbor, visited, color)) {
+                    res = false;
+                    break;
+                }
+            } else if (visited[neighbor] && color[neighbor] == color[cur]) {
+                res = false;
+                break;
+            }
+        }
+        return res;
     }
 }
--- src/DFS/CyclesTest.java
+++ src/DFS/CyclesTest.java
@@ -4,15 +4,13 @@ import Graph.Graph;
 
 public class CyclesTest {
     public static void main(String[] args) {
-        Graph graph = new Graph(9);
+        Graph graph = new Graph(5);
         graph.addEdge(0, 1);
         graph.addEdge(1, 2);
         graph.addEdge(2, 3);
-        graph.addEdge(3, 4);
-        graph.addEdge(4, 5);
-        graph.addEdge(4, 7);
-        graph.addEdge(4, 6);
+        graph.addEdge(3, 0);
+//        graph.addEdge(4, 0);
 
-        System.out.println(Cycles.hasCycles(graph));
+        System.out.println(Cycles.isBipartite(graph)) ;
     }
 }
